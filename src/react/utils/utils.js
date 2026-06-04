2cb1cfeaaa23cc0dbf18c38378c3329ba9d5e2db
Afnan Khalid <afnank19@gmail.com>
2026-06-05 01:48:45 +0500

    feat: add ansi conversion, html escaping

--- src/react/utils/utils.js
+++ src/react/utils/utils.js
@@ -9,3 +9,12 @@ export function getFolderName(folderPath) {
     .split(/[\\/]/) // split on both separators
     .pop();
 }
+
+export function escapeHtml(str) {
+  return str
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&#039;");
+}
